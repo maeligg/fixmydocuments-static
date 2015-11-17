5857d355f78fd38717f4065629caa75a026c99df
Maëlig <maelbrunet@gmail.com>
2015-11-17 16:26:30 +0100

    redesign of the cards using the featherlight jQuery plugin

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(function() {
+  $('.card').click(function(){
+    $('.card').removeClass('card--big').addClass('card--small');
+    $(this).toggleClass('.card--small card--big');
+  });
   $('.card').flip(); 
 });
